e08c910c12512f1fbd55e4db9447f7607dae0990
dm1triykr <dimonk601@gmail.com>
2022-09-26 19:40:20 +0300

    feat: 'Meet The Team' block, 'Categories' block, 'Follow us' block, 'Retailer Stores' block

--- build/js/app.js
+++ build/js/app.js
@@ -23,6 +23,41 @@ $(document).ready(function () {
     arrows: true,
     slidesToShow: 2
   });
+  $('.meet-with-team-slider__tabs').slick({
+    dots: false,
+    arrow: false,
+    slidesToShow: 3,
+    asNavFor: '.meet-with-team-slider__slides',
+    focusOnSelect: true
+  });
+  $('.meet-with-team-slider__wrapper').slick({
+    customPaging: function (slider, i) {
+      var slidernav = $(slider.$slides[i]).data('slidernav');
+      return '<a>' + slidernav + '</a>';
+    },
+    dots: true,
+    //speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+  $('.categories-slider__slides').slick({
+    arrows: false,
+    dots: false,
+    slidesToShow: 5,
+    // centerMode: true,
+    infinite: false,
+    //initialSlide: 2,
+    responsive: [{
+      breakpoint: 1441,
+      settings: {
+        slidesToShow: 4.22
+      }
+    }]
+  });
+  $('.retailer-stores-slider__slides').slick({
+    dots: true,
+    arrows: false
+  });
 });
 const swiper = new Swiper('.swiper', {
   // Optional parameters
@@ -38,5 +73,28 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev'
   } // And if we need scrollbar
 
-});
+}); // function openTab(evt, tabName) {
+// 	// Declare all variables
+// 	var i, tabcontent, tablinks;
+//
+// 	// Get all elements with class="tabcontent" and hide them
+// 	tabcontent = document.getElementsByClassName("tab__content");
+// 	console.log(tabcontent);
+// 	for (i = 0; i < tabcontent.length; i++) {
+// 		tabcontent[i].style.display = "none";
+// 	}
+//
+// 	// Get all elements with class="tablinks" and remove the class "active"
+// 	tablinks = document.getElementsByClassName("tab__link");
+// 	console.log(tablinks);
+// 	for (i = 0; i < tablinks.length; i++) {
+// 		tablinks[i].className = tablinks[i].className.replace(" active", "");
+// 	}
+//
+// 	// Show the current tab, and add an "active" class to the button that opened the tab
+// 	document.getElementById(tabName).style.display = "block";
+// 	evt.currentTarget.className += " active";
+//
+// 	$('#'+tabName).find('.meet-with-team-slider__slides:not(.refreshed)').slick('reinit').addClass('refreshed');
+// }
 //# sourceMappingURL=app.js.map
